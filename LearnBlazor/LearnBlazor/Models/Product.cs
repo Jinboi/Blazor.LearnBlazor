ee5382d0c3637701d380068812e9c5888b70e3ff
Jinboi <duddnwkfgo7@gmail.com>
2024-10-23 07:55:05 -0700

    added product_prop

--- LearnBlazor/LearnBlazor/Models/Product.cs
+++ LearnBlazor/LearnBlazor/Models/Product.cs
@@ -6,6 +6,8 @@
         public  string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+
 
     }
 }
